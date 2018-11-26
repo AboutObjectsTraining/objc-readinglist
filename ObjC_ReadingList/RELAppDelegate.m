43a546b38e6e7f447303f88cd28f462cd06a6c2b
Jonathan Lehr <jonathan@aboutobjects.com>
2018-11-25 20:08:36 -0800

    Added code comments

--- ObjC_ReadingList/RELAppDelegate.m
+++ ObjC_ReadingList/RELAppDelegate.m
@@ -7,20 +7,37 @@
 
 @implementation RELAppDelegate
 
-- (NSDictionary *)titleAttributes {
+/// Returns visual attributes for the navigation bar's title
+- (NSDictionary *)navBarTitleAttributes {
     return @{ NSForegroundColorAttributeName : [UIColor colorNamed:@"Title Color"] };
 }
 
 - (void)applicationDidFinishLaunching:(UIApplication *)application
 {
+    [self configureGlobalAppearanceSettings];
+}
+
+/**
+ Configures `UIAppearance` proxies for several view classes, and sets the window's
+ tint color.
+ */
+- (void)configureGlobalAppearanceSettings {
     self.window.tintColor = [UIColor colorNamed:@"Tint Color"];
     
-    UINavigationBar.appearance.titleTextAttributes = self.titleAttributes;
-    UINavigationBar.appearance.largeTitleTextAttributes = self.titleAttributes;
+    UINavigationBar.appearance.titleTextAttributes = self.navBarTitleAttributes;
+    UINavigationBar.appearance.largeTitleTextAttributes = self.navBarTitleAttributes;
     
     UITableView.appearance.backgroundColor = [UIColor colorNamed:@"Alternate Cell Color"];
     [UITableViewCell appearanceWhenContainedInInstancesOfClasses:
      @[[RELEditBookController class], [RELAddBookController class]]].backgroundColor = [UIColor colorNamed:@"Cell Color"];
+}
+
+/**
+ Overridden to send `resignFirstResponder` to the current first responder, if any.
+ */
+- (void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event
+{
+    [UIApplication.sharedApplication sendAction:@selector(resignFirstResponder) to:nil from:nil forEvent:nil];
 }
 
 @end
--- ObjC_ReadingList/UIStoryboardSegue+RELAdditions.h
+++ ObjC_ReadingList/UIStoryboardSegue+RELAdditions.h
@@ -5,6 +5,10 @@
 
 @interface UIStoryboardSegue (RELAdditions)
 
+/**
+ If the controller we're interested in is embdedded in a navigation controller, returns
+ the navigation controller's first child. Otherwise returns `destinationViewController`.
+ */
 @property (readonly, nonatomic) UIViewController *rel_destinationViewController;
 
 @end
