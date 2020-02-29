95c7c7b418a1308ca21a9354097c903936ae3dc4
Jonathan Lehr <jonathan@aboutobjects.com>
2020-02-28 16:05:38 -0800

    Added UIColor+RELAdditions

---
+++ ObjC_ReadingList/UIColor+RELAdditions.h
@@ -0,0 +1,17 @@
+// Copyright (C) 2019 About Objects, Inc. All Rights Reserved.
+// See LICENSE.txt for this project's licensing information.
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface UIColor (RELAdditions)
+
+@property (class, readonly, nonatomic) UIColor *titleColor;
+@property (class, readonly, nonatomic) UIColor *tintColor;
+@property (class, readonly, nonatomic) UIColor *cellColor;
+@property (class, readonly, nonatomic) UIColor *alternateCellColor;
+
+@end
+
+NS_ASSUME_NONNULL_END
