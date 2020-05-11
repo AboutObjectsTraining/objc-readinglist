147716d4751275f5c93cd50e39a8c43fcaff85a2
Jonathan Lehr <jonathanlehr@users.noreply.github.com>
2020-05-11 13:10:44 -0400

    Recent updates (#2)
    * Added UIColor+RELAdditions
    * Simplified Done and Cancel segues
    * Cleanups; renamed global constant
    * Reload data on deletion with delayed perform
    * Populate missing author object
    * Dark mode color fixes

--- ObjC_ReadingList/RELEditBookController.m
+++ ObjC_ReadingList/RELEditBookController.m
@@ -9,13 +9,26 @@
 @property (weak, nonatomic) IBOutlet UITextField *yearField;
 @property (weak, nonatomic) IBOutlet UITextField *firstNameField;
 @property (weak, nonatomic) IBOutlet UITextField *lastNameField;
+@property (readonly, nonatomic) BOOL hasAuthorInput;
 @end
 
 @implementation RELEditBookController
 
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    
+    if (@available(iOS 13, *)) {
+        self.modalInPresentation = true;
+    }
+}
+
 - (void)viewWillAppear:(BOOL)animated
 {
     [super viewWillAppear:animated];
+    [self populateBook];
+}
+
+- (void)populateBook {
     self.titleField.text = self.book.title;
     self.yearField.text = self.book.year;
     self.firstNameField.text = self.book.author.firstName;
@@ -24,10 +37,22 @@
 
 - (void)updateBook
 {
+    if (!self.book.author && self.hasAuthorInput) {
+        self.book.author = [[RLMAuthor alloc] init];
+    }
+    
     self.book.title = self.titleField.text;
     self.book.year = self.yearField.text;
     self.book.author.firstName = self.firstNameField.text;
     self.book.author.lastName = self.lastNameField.text;
+}
+
+- (BOOL)hasAuthorInput {
+    NSString *firstName = [self.firstNameField.text
+                           stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];
+    NSString *lastName = [self.lastNameField.text
+                          stringByTrimmingCharactersInSet:NSCharacterSet.whitespaceCharacterSet];
+    return firstName.length > 0 || lastName.length > 0;
 }
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
